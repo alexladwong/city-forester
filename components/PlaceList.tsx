028339e3e4e35309ef54619eae95fc2eefe7beb7
alexladwong <alexladwong@gmail.com>
2025-01-29 19:13:29 +0300

    favicon/Decs/footer006

--- components/PlaceList.tsx
+++ components/PlaceList.tsx
@@ -47,9 +47,11 @@ function PlaceList({ placeList }: PlaceListProps) {
           />
         </div>
       ) : null}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        
-      </div>
+      {placeList?.length===0?<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {[1,2,3,4,5,6,7,8,9,10].map((item,index) =>(
+          <Skelton />
+        ))}
+      </div>:null}
     </div>
   );
 }
